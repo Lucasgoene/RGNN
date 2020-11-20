fix softmax in domain output

model_simple.py
@@ -120,7 +120,7 @@ class SymSimGCNNet(torch.nn.Module):
         self.conv1 = NewSGConv(num_features=num_features, num_classes=num_hiddens[0], K=K)
         self.fc = nn.Linear(num_hiddens[0], num_classes)
         if self.domain_adaptation in ["RevGrad"]:
-            self.domain_classifier = F.softmax(nn.Linear(num_hiddens[0], 2))
+            self.domain_classifier = nn.Linear(num_hiddens[0], 2)
 
     def forward(self, data, alpha=0):
         batch_size = len(data.y)
@@ -136,6 +136,7 @@ class SymSimGCNNet(torch.nn.Module):
         if self.domain_adaptation in ["RevGrad"]:
             reverse_x = ReverseLayerF.apply(x, alpha)
             domain_output = self.domain_classifier(reverse_x)
+            domain_output = F.softmax(domain_output)
         x = global_add_pool(x, data.batch, size=batch_size)
         x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.fc(x)